51dfc7a227472e403260cd2f0376f44a3a67e05a
Tilde Twisttmann <tihe001@stud.kea.dk>
2024-10-08 14:07:04 +0200

    hh

---
+++ 3caPitalization/js.js
@@ -0,0 +1,4 @@
+const myName = "Tilde Twisttmann";
+
+const third = (myName.substring(0,2).toLowerCase() + myName.substring(3, 1).toUpperCase() + myName.substring(3).toLowerCase());
+console.log (third)
